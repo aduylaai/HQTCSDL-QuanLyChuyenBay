6ca3421f29383aa62068ae433c57810338599835
ak090804 <khoadoan09082004@gmail.com>
2024-11-04 14:58:23 +0700

    cm program

--- QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Program.cs
+++ QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuanLyChuyenBay_Demo.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace QuanLyChuyenBay_Demo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmDangNhap());
         }
     }
 }
