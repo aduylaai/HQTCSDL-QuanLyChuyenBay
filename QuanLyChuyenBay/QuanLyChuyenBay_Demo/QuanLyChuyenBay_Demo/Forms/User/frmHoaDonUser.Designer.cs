f73ee8217f561d50842d47a5c44eea09f3a74096
aduylaai <aduylaai@gmail.com>
2024-11-12 11:09:05 +0700

    form

--- QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Forms/User/frmHoaDonUser.Designer.cs
+++ QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Forms/User/frmHoaDonUser.Designer.cs
@@ -30,25 +30,52 @@ namespace QuanLyChuyenBay_Demo.Forms.User
         private void InitializeComponent()
         {
             this.pnlBody = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlBody.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlBody
             // 
+            this.pnlBody.Controls.Add(this.label2);
+            this.pnlBody.Controls.Add(this.label1);
             this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlBody.Location = new System.Drawing.Point(0, 0);
             this.pnlBody.Name = "pnlBody";
-            this.pnlBody.Size = new System.Drawing.Size(800, 450);
+            this.pnlBody.Size = new System.Drawing.Size(1160, 594);
             this.pnlBody.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Hóa Đơn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1018, 563);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hóa đơn";
             // 
             // frmHoaDonUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1160, 594);
             this.Controls.Add(this.pnlBody);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmHoaDonUser";
             this.Text = "frmHoaDonUser";
+            this.pnlBody.ResumeLayout(false);
+            this.pnlBody.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +83,7 @@ namespace QuanLyChuyenBay_Demo.Forms.User
         #endregion
 
         private System.Windows.Forms.Panel pnlBody;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
     }
 }
--- QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Forms/User/frmThongTinCaNhan.Designer.cs
+++ QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Forms/User/frmThongTinCaNhan.Designer.cs
@@ -30,25 +30,52 @@ namespace QuanLyChuyenBay_Demo.Forms.User
         private void InitializeComponent()
         {
             this.pnlBody = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlBody.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlBody
             // 
+            this.pnlBody.Controls.Add(this.label2);
+            this.pnlBody.Controls.Add(this.label1);
             this.pnlBody.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlBody.Location = new System.Drawing.Point(0, 0);
             this.pnlBody.Name = "pnlBody";
-            this.pnlBody.Size = new System.Drawing.Size(800, 450);
+            this.pnlBody.Size = new System.Drawing.Size(1160, 594);
             this.pnlBody.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Thông tin cá nhân";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1018, 563);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Thông tin cá nhân";
             // 
             // frmThongTinCaNhan
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1160, 594);
             this.Controls.Add(this.pnlBody);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmThongTinCaNhan";
             this.Text = "frmThongTinCaNhan";
+            this.pnlBody.ResumeLayout(false);
+            this.pnlBody.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +83,7 @@ namespace QuanLyChuyenBay_Demo.Forms.User
         #endregion
 
         private System.Windows.Forms.Panel pnlBody;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
