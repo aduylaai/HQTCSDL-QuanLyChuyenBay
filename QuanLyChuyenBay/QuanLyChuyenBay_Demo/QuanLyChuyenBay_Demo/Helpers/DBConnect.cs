729e5f8865068a2c682ccfa6938c8db250ce54d9
minhluan2421 <nguyenminhluan0824@gmail.com>
2024-11-13 22:59:41 +0700

    cm

--- QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Helpers/DBConnect.cs
+++ QuanLyChuyenBay/QuanLyChuyenBay_Demo/QuanLyChuyenBay_Demo/Helpers/DBConnect.cs
@@ -59,7 +59,7 @@ namespace QuanLyChuyenBay_Demo.Helpers
 
         public DBConnect()
         {
-            strServerName = "ADUYLAAI"; strDBName = "QuanLyBanVeMayBay";
+            strServerName = "DESKTOP-360CGGJ"; strDBName = "QuanLyBanVeMayBay";
 
             strConnect = @"Data Source=" + strServerName + ";Initial Catalog=" + strDBName + ";Integrated Security=true";
             conn = new SqlConnection(strConnect); //Khởi tạo đối tượng kết nối đến CSDL
